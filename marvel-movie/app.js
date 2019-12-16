d1bd9b8c3b7370f2dc4d453e2676275d6c0490f9
lagrassanicolas <la.grassa.nicolas@iisgalvanimi.edu.it>
2019-12-16 10:34:18 +0000

    punto3

--- marvel-movie/app.js
+++ marvel-movie/app.js
@@ -1,9 +1,18 @@
 
 var express = require('express');
-var app = express(); 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+const film =  require ('./film.json');
+const app = express();
+const path = require ("path");
+
+
+app.set('view engine','pug');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/api/movies', function (req, res) {
+  res.send(film);
 });
+  
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
